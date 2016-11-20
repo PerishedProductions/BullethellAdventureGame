582a9483e9b50a8429cf1e30ff04dc342d0cef49
Mikkel Mouridsen <mikkelmouridsen@me.com>
2016-11-20 14:13:49 +0100

    Added json loading

--- BullethellAdventureGame/GameLevels/MainLevel.cs
+++ BullethellAdventureGame/GameLevels/MainLevel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -9,6 +10,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 using CoreGame.Entities;
+using CoreGame.Utilities;
 
 namespace CoreGame.GameLevels
 {
@@ -21,6 +23,10 @@ namespace CoreGame.GameLevels
         {
             player = new Player();
             player.Initialize();
+
+            ReadJson jsonReader = new ReadJson();
+            Debug.WriteLine(jsonReader.ReadData("Data/Data.json")["name"]);
+            Debug.WriteLine(jsonReader.ReadData("Data/Data.json")["age"]);
         }
 
         public override void LoadContent(ContentManager content)
